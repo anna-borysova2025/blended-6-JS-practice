837bf8e36a4d47bba378029703a0167cde413244
anna_borysova <anna@pavlyuk.org>
2025-10-02 16:37:20 +0300

    add tasks

--- js/lesson1.js
+++ js/lesson1.js
@@ -2,6 +2,23 @@
 // // alert("Привет");
 // prompt("login");
 
+// Запропонуйте користувачу ввести число за допомогою prompt().
+// Збережіть введене значення у змінну number.
+// Перевірте, чи дорівнює змінна number числу 10:
+//    Якщо так – виведіть повідомлення 'Вірно' у alert().
+//    Якщо ні – виведіть 'Невірно'.
+
+// Підказка: prompt завжди повертає рядок (String), тому перед перевіркою
+// перетворіть значення на число за допомогою Number().
+
+// let number = prompt("введіть число");
+
+// if (+number === 10) {
+//   alert("вірно");
+// } else {
+//   alert("невірно");
+// }
+
 // У змінній min лежить випадкове число від 0 до 59.
 // Визначте, в яку чверть години потрапляє
 // це число (у першу, другу, третю чи четверту).
@@ -9,13 +26,17 @@
 // "10 входить в першу чверть"
 
 // const min = Math.floor(Math.random() * (59 - 0) + 0);
-// if (min >= 0 && min < 15) {
-//   alert(min + " входить в першу чверть");
-// } else if (min >= 15 && min < 30) {
-//   alert(min + " входить в другу чверть");
-// } else if (min >= 30 && min < 45) {
-//   alert(min + " входить в третю чверть");
-// } else {
+// if (min <= 14) {
+//   alert(`${min} входить в першу чверть`);
+// }
+// if (min >= 15 && min <= 29) {
+//   alert(`${min} входить в другу чверть`);
+// }
+// if (min >= 30 && min <= 44) {
+//   alert(`${min} входить в третю чверть`);
+// }
+// if (min >= 45 && min <= 59) {
+//   alert(`${min} входить в четверту чверть`);
 // }
 
 // Змінна num може набувати 4 значення: '1', '2', '3' або '4'
@@ -26,4 +47,366 @@
 // Не забудьте про дефолтне значення, на випадок, якщо користувач
 // введе в prompt щось інше. В такому випадку result має набувати значення:
 // "Вибачте, але ви маєте ввести значення від 1 до 4 включно".
-// Значення змінної result виведіть в консоль.
+// // Значення змінної result виведіть в консоль.
+
+// let num = prompt("введіть значення від 1 до 4");
+
+// switch (num) {
+//   case "1":
+//     result = "зима";
+//     break;
+//   case "2":
+//     result = "весна";
+//     break;
+//   case "3":
+//     result = "літо";
+//     break;
+//   case "4":
+//     result = "осінь";
+//     break;
+//   default:
+//     result = "Вибачте, але ви маєте ввести значення від 1 до 4 включно";
+// }
+
+// alert(result);
+
+// Напишіть код, який запитуватиме у користувача
+// логін за допомогою prompt і виводить результат в консоль браузера
+
+// Після цього додайте перевірку введеного користувачем значення:
+// Якщо відвідувач вводить "Адмін",
+// то prompt запитує пароль (не забудьте зберігти його у змінну для подальшої перевірки).
+// Якщо нічого не ввели або натиснули Cancel,
+// Вивести в alert строку "Скасовано"
+// В іншому випадку вивести в alert рядок "Я вас не знаю"
+
+// Пароль перевіряти так:
+// Якщо введено пароль "Я головний",
+// то вивести в alert рядок "Добрий день!",
+// в іншому випадку вивести в alert рядок "Невірний пароль!"
+// let login = prompt("Введіть логін");
+// if (login === "Адмін") {
+//   let password = prompt("Введіть пароль");
+//   if (password === "Я головний") {
+//     alert("Добрий день!");
+//   } else {
+//     alert("Невірний пароль!");
+//   }
+// } else if (login === null || login === "") {
+//   alert("Скасовано");
+// } else {
+//   alert("Я вас не знаю");
+// }
+
+// Дано число. Проверьте, отрицательное оно или нет. Выведите об этом информацию в консоль.
+
+// let number = prompt("Введите число");
+// if (number < 0) {
+//   console.log("Число отрицательное");
+// } else {
+//   console.log("Число не отрицательное");
+// }
+
+// Перебери об'єкт apartment, використовуючи цикл for...in, і запиши в масив keys всі його ключі, а в масив values всі значення його властивостей.
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const keys = [];
+// const values = [];
+// for (const key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+// Перебери об'єкт apartment, використовуючи метод Object.keys() і цикл for...of.
+// Запиши у змінну keys масив ключів властивостей об'єкта apartment,
+// і додай в масив values всі значення його властивостей.
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// const keys = Object.keys(apartment);
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
+
+// Виконай рефакторинг функції countProps(object), замінивши перебір ключів за допомогою циклу for…in на метод Object.keys()
+// для отримання масиву властивостей. Функція має повернути кількість властивостей в об'єкті object.
+// function countProps(object) {
+//   const keys = Object.keys(object);
+//   return keys.length;
+// }
+
+// for (const key in object) {
+//   if (object.hasOwnProperty(key)) {
+//     propCount += 1;
+//   }
+// }
+
+// return propCount;
+
+// Функція countTotalSalary(salaries) приймає об'єкт зарплат (salaries) в якості параметра. Кожна властивість об'єкта salaries — це ключ, що містить ім'я співробітника, та значення - його відповідна зарплатня.
+
+// Доповни код функції countTotalSalary(salaries) так, щоб вона повертала загальну суму зарплат всіх співробітників.
+
+// Поради:
+
+// Ініціалізуй змінну totalSalary зі значенням 0, яка буде відповідати за загальну суму зарплат усіх співробітників
+// Використай метод Object.values() для отримання значень (зарплат) з об'єкта salaries
+// Пройдись по отриманих значеннях за допомогою циклу та додай кожне значення до змінної totalSalary.
+// Поверни totalSalary як результат
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+// }
+// const values = Object.values(salaries);
+// for (const value of values) {
+//   totalSalary += value;
+// }
+// return totalSalary;
+// Масив colors містить колекцію кольорів. Кожен колір представлений об'єктом і має властивості hex і rgb з відповідними для цього формату і кольору значеннями.
+
+// Перебери масив об'єктів colors, використовуючи цикл for...of.
+// Додай у масив hexColors значення властивостей hex, а в масив rgbColors - значення властивостей rgb з усіх об'єктів масиву colors.
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+
+// Функція getProductPrice(productName) приймає один параметр productName - назва продукту.
+// Функція містить масив об'єктів products з такими властивостями, як name — ім'я товару, price — ціна і quantity — кількість.
+
+// Доповни код функції так, щоб вона шукала об'єкт продукту з певним ім'ям (властивість name) в масиві products і повертала його ціну (властивість price).
+// Якщо продукт з такою назвою не знайдений, функція повинна повертати null.
+
+// function getProductPrice(productName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price;
+//     }
+//   }
+//   return null;
+// }
+
+// Напиши функцію getAllPropValues(propName), яка приймає один параметр propName - ім'я (ключ) властивості. Функція повинна повернути масив усіх значень властивості
+//  з таким ім'ям з кожного об'єкта в масиві products. Якщо в об'єктах відсутні властивості з таким ім'ям, функція повинна повернути порожній масив.
+
+// function getAllPropValues(propName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+
+//   const values = [];
+//   for (const product of products) {
+//     if (product.hasOwnProperty(propName)) {
+//       values.push(product[propName]);
+//     }
+//   }
+//   return values;
+// }
+
+// Функція calculateTotalPrice(productName) приймає один параметр productName- назва товару.
+// Функція містить масив об'єктів products з такими властивостями, як name — ім'я товару, price — ціна і quantity — кількість.
+
+// Доповни код функції так, щоб вона повертала загальну вартість (ціна * кількість) товару з таким ім'ям з масиву products.
+
+// Якщо продукту з такою назвою немає, то функція повинна повертати рядок "Product <productName> not found!" , де <productName> — це ім'я товару.
+
+// function calculateTotalPrice(productName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price * product.quantity;
+//     }
+//   }
+//   return `Product ${productName} not found!`;
+// }
+
+// Об'єкт atTheOldToad має наступні властивості:
+
+// potions — масив рядків, з назвами зілль
+// getPotions() — метод, який повертає рядок "List of all available potions"
+// Зміни код метода об'єкта getPotions() так, щоб він повертав значення властивості potions
+
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Stone skin"],
+//   getPotions() {
+//     return this.potions;
+//   },
+// };
+// Об'єкт atTheOldToad має наступні властивості:
+
+// potions — масив рядків, з назвами зілль
+// getPotions() — метод, який повертає значення властивості potions
+// addPotion() — метод, який приймає параметр рядок з назвою зілля potionName
+// Зміни код методу addPotion(potionName) так, щоб він додавав зілля potionName в кінець масиву у властивості potions.
+
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Stone skin"],
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(potionName) {
+//     this.potions.push(potionName);
+//   },
+// };
+
+// Замовниця хоче, щоб кожне зілля було представлено не тільки ім'ям, але й ціною. Тому зараз у властивості potions буде зберігатися масив об'єктів з властивостями name та price.
+
+// Об'єкт atTheOldToad має наступні властивості:
+
+// potions — масив рядків, з назвами зілль
+// getPotions() — метод, який повертає значення властивості potions
+// addPotion() — метод, який приймає параметр об'єкт нового зілля newPotion і додає його в кінець масиву у властивості potions.
+// Додай метод getTotalPrice(), який має повертати загальну вартість усіх зілль з властивості potions.
+
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Stone skin", price: 520 },
+//   ],
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(newPotion) {
+//     this.potions.push(newPotion);
+//   },
+//   getTotalPrice() {
+//     let total = 0;
+//     for (const potion of this.potions) {
+//       total += potion.price;
+//     }
+//     return total;
+//   },
+// };
+// Об'єкт atTheOldToad має наступні властивості:
+
+// potions — масив об'єктів зілль
+// getPotions() — метод, який повертає значення властивості potions
+// updatePotionName() — метод, який приймає два параметра рядків oldName і newName
+// Доповни метод updatePotionName(oldName, newName) таким чином, щоб він оновлював назву зілля з oldName на newName в масиві зілля у властивості potions.
+
+//     { name: "Speed potion", price: 460 },
+//     { name: "Stone skin", price: 520 },
+//   ],
+//   getPotions() {
+//     return this.potions;
+//   },
+//   updatePotionName(oldName, newName) {
+//     for(const potion of this.potions){
+//       if(potion.name === oldName){
+//         potion.name = newName;
+//       }
+//     }
+//   },
+// };
+
+// Використовуючи синтаксис залишкових параметрів, доповни код функції add() так, щоб вона приймала будь-яку кількість аргументів у параметр args і повертала їхню суму.
+// function add(...args) {
+//     let total = 0;
+//     for (const arg of args) {
+//         total += arg;
+//     }
+//     return total;
+// }
+
+// Функція addOverNum() приймає довільну кількість аргументів чисел.
+
+// Доповни код функції таким чином, щоб вона обчислювала суму тільки тих аргументів, які більші за задане число. Це число завжди буде передано першим аргументом.
+
+// Для вирішення цього завдання тобі потрібно зробити наступне:
+
+// Перший параметр value повинен представляти задане число, а решта аргументів повинні бути зібрані за допомогою синтаксису (...args)
+// Усередині функції ініціалізуй змінну для зберігання загальної суми
+// Потім пройдись по кожному аргументу за допомогою циклу
+// Перевір, чи кожен аргумент більший за вказане число, і якщо так, додай його до загальної суми
+// На завершення поверни загальну суму
+// function addOverNum(value, ...args) {
+//     let total = 0;
+//     for (const arg of args) {
+//         if (arg > value) {
+//             total += arg;
+//         }
+//     }
+//     return total;
+// }
+
+// Функція getExtremeScores(scores) приймає масив оцінок (чисел) у параметрі scores.
+
+// Доповни код функції так, щоб вона повертала об'єкт із двома властивостями:
+
+// Властивість best має містити найбільше число з масиву scores
+// Властивість worst має містити найменше число з масиву scores.
+// Використовуй оператор (...spread) і методи Math.max() і Math.min().
+
+// function getExtremeScores(scores) {
+//     return {
+//         best: Math.max(...scores),
+//         worst: Math.min(...scores),
+//     };
+// }
+
+// У змінних firstGroupScores, secondGroupScores і thirdGroupScores зберігаються результати тестування окремих груп. Використовуючи розпилення, доповни код таким чином, щоб:
+
+// У змінній allScores зберігався масив всіх результатів від першої до третьої групи включно.
+// У змінній bestScore був найвищий загальний бал.
+// У змінній worstScore був найнижчий загальний бал.
+
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+
+// const allScores = [];
+// const bestScore = allScores;
+// const worstScore = allScores;
+
+// allScores.push(...firstGroupScores, ...secondGroupScores, ...thirdGroupScores);
+// bestScore = Math.max(...allScores);
+// worstScore = Math.min(...allScores);
+
+// В конструкторі можна створювати нові тести, для яких є налаштування за замовчуванням, які зберігаються у змінній defaultSettings.
+// Під час створення тесту, усі або частину налаштувань можна перевизначити, користувацькі налаштування зберігаються у змінній overrideSettings.
+
+// Для того щоб отримати фінальні налаштування тесту, необхідно взяти налаштування за замовчуванням і поверх них застосувати перевизначені користувацькі налаштування.
+// Доповни код таким чином, щоб у змінній finalSettings утворився об'єкт фінальних налаштувань тесту.
+
+// const defaultSettings = {
+//   theme: "light",
+//   public: true,
+//   withPassword: false,
+//   minNumberOfQuestions: 10,
+//   timePerQuestion: 60,
+// };
+// const overrideSettings = {
+//   public: false,
+//   withPassword: true,
+//   timePerQuestion: 30,
+// };
+
+// const finalSettings = { ...defaultSettings, ...overrideSettings };
